861f960975baa3401c6aeffdb63221be59c44223
hiroto.ohira <hiroto100114@gmail.com>
2025-05-06 19:01:57 +0900

    🎉 Fix: Open today's Notion page with daily tasks
    - Added open.default(dailyPageUrl) to open today's Notion page
    - Added function to fetch today's date and use it in the search query
    - Added a function to get the url of the page

--- searchApi.js
+++ searchApi.js
@@ -1,19 +1,49 @@
+require('dotenv').config()
+const open = require('open');
 const { Client } = require('@notionhq/client');
 
+// Notion client
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
+// Default open page
+const dailyPageUrl = 'https://www.notion.so/Daily-36bd146c72f14de5920f9a6ee6b2ad4c';
+
+const padTwoDigits = (num) => {
+  return String(num).padStart(2, '0');
+}
+// Like '2025-05-06'
+const today = (() => {
+  const now = new Date()
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+
+  return `${year}-${padTwoDigits(month)}-${padTwoDigits(day)}`
+})();
+
+const getTitle = function(page) {
+  return page.properties.title.title[0].text.content
+};
+const getUrl = function(page) {
+  return page.url
+};
+
+// Open today page.
 (async () => {
+  // see https://developers.notion.com/reference/post-search
   const response = await notion.search({
-    query: 'External tasks',
-    filter: {
-      value: 'database',
-      property: 'object'
-    },
+    query: today,
     sort: {
-      direction: 'ascending',
+      direction: 'descending',
       timestamp: 'last_edited_time'
     },
   });
-  console.log(response);
+
+  const todayPage = response.results.find(page => {
+    return getTitle(page) === today
+  })
+
+  const url = todayPage === undefined ? dailyPageUrl : getUrl(todayPage)
+  await open.default(url)
 })();
 
